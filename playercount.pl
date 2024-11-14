51cbc63a4f4f7a8dfe4bf001065c35495bcadb4c
dukesix144 <rheinisch@intacthost.com>
2024-11-13 20:30:57 -0600

    Update playercount.pl

--- playercount.pl
+++ playercount.pl
@@ -1,9 +1,5 @@
 #!/usr/bin/perl
 
-# $ qstat -hls 209.204.62.175:27019
-# ADDRESS           PLAYERS      MAP   RESPONSE TIME    NAME
-# 209.204.62.175:27019  12/21    ns_tanith      8 / 0  nsp NS 3.0beta3 LerkLift-HealArmory-PhaseEQ@ warmfuzzyland.com
-
 
 $response = `curl -s https://www.rrgaming.net/api/playercount81.php`;
 
